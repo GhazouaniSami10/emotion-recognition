d0224b112eec18f47b136702b0c4c5c5447e3876
root <root@ghazouanisami100stic@gmail.com>
2020-04-09 00:07:35 +0100

    first commit

---
+++ emotion_recognizer_webapp/constants.py
@@ -0,0 +1,8 @@
+class Constant:
+
+	#MODEL_PATH = '/home/nikhil/Downloads/saved_models/Emotion_Voice_Detection_Model.h5'
+	#LABELS_PATH = '/home/nikhil/Downloads/label_classes.npy'
+	#INPUT_AUDIO_PATH = '/home/nikhil/personal/emotion_app/live_audio.wav'
+	#MODEL_PATH = '/home/gladiateur/Desktop/emotion-recognition/saved_models/Emotion_Voice_Detection_Model.h5'
+	#LABELS_PATH = '/home/gladiateur/Desktop/emotion-recognition/label_classes.npy'
+	INPUT_AUDIO_PATH = '/home/gladiateur/Desktop/emotion-recognition/emotion_recognizer_webapp/live_audio.wav'
